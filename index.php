6137cb7ab654e6d68c7775845da106697e754722
ranjeet <1705213032@ietlucknow.ac.in>
2020-02-27 18:58:53 +0530

    update at index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+</head>
+<body>
+    
+    <h3 align ="right" style="margin-right:20px;"><a href="login.php">Admin Login</a></h5>
+    <h1 align="center">Welcome to Student Management System </h1>
+    <form method="post" action="index.php">
+    <table>
+        <tr>
+            <td colspan ="2">Student Information</td>
+        </tr>
+        <tr>
+            <td>Choose Standad</td>
+            <td>
+                <select>
+                    <option value="1">1st</option>
+                    <option value="2">2nd</option>
+                    <option value="3">3rd</option>
+                    <option value="4">4th</option>
+                    <option value="5">5th</option>
+                    <option value="6">6th</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Enter Rollno</td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
