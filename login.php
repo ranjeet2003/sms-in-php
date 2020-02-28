43d08d78ef8742ef9926392f385086a56c81b6fd
ranjeet <1705213032@ietlucknow.ac.in>
2020-02-28 14:17:48 +0530

    login details is matched from database

--- login.php
+++ login.php
@@ -6,9 +6,9 @@
     <title>Admin Login</title>
 </head>
 <body>
-    <h1>Admin Login</h1>
+    <h1 align="center">Admin Login</h1>
     <form action="login.php" method="post">
-        <table>
+        <table align ="center"border="2">
             <tr>
                 <td>Username</td><td><input type="text" name="uname" required></td>
             </tr>
@@ -24,3 +24,35 @@
     </form>
 </body>
 </html>
+<?php
+    include('dbcon.php');
+    if(isset($_POST['login'])){
+        $username=$_POST['uname'];
+        $password=$_POST['pass'];
+        $qry="SELECT * FROM `admin` WHERE `username`='$username' AND `password`='$password'";
+        $run=mysqli_query($con,$qry);
+        $row=mysqli_num_rows($run);
+        if($row<1){
+            ?>
+            <script>
+                alert('Username or Password is incorrect !');
+                window.open('login.php','_self');
+            </script>
+
+            <?php
+        }
+        else{
+            $data=mysqli_fetch_assoc($run);
+            $id=$data['id'];
+            echo "id=".$id;
+        }
+
+
+
+
+
+    }
+
+
+
+?>
