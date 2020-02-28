c5472e112ed0ce4dc5ebb22ef1ab052f724fbc3a
ranjeet <1705213032@ietlucknow.ac.in>
2020-02-28 23:58:04 +0530

    tile head is created

---
+++ admin/titlehead.php
@@ -0,0 +1,5 @@
+<div class="admintitle" align ="center">
+    <h4><a href="logout.php" style ="float:right; margin-right:30px; color:#fff; font-size:20px;">Logout</a></h4>
+   <h1>Welcome to Admin Dashboarrd</h1>
+  
+</div>
