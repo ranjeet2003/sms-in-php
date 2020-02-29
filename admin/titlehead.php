e1e47c958b209ab7721249b448e7bda714274cbb
ranjeet <1705213032@ietlucknow.ac.in>
2020-02-29 12:07:16 +0530

    titlehead is created

--- admin/titlehead.php
+++ admin/titlehead.php
@@ -1,5 +1,8 @@
 <div class="admintitle" align ="center">
-    <h4><a href="logout.php" style ="float:right; margin-right:30px; color:#fff; font-size:20px;">Logout</a></h4>
+    <h4>
+        <a href="admindash.php" style ="float:left; margin-right:30px; color:#fff; font-size:20px;">Back To Dashboard</a>
+        <a href="logout.php" style ="float:right; margin-right:30px; color:#fff; font-size:20px;">Logout</a>
+    </h4>
    <h1>Welcome to Admin Dashboarrd</h1>
   
 </div>
