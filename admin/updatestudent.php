19163c8b591b9da32b9da7ddf209379c3eb74ad2
ranjeet <1705213032@ietlucknow.ac.in>
2020-02-29 12:55:53 +0530

    details is fetched succesfully

---
+++ admin/updatestudent.php
@@ -0,0 +1,68 @@
+<?php
+    session_start();
+   if(isset($_SESSION['uid'])){
+       echo "";
+   }
+  else{
+      header('location:../login.php');
+  }
+?>
+<?php
+include('header.php');
+include('titlehead.php');
+?>
+<table align="center">
+<form action="updatestudent.php" method="post">
+<tr>
+    <th>Enter Standerd<td> <input type="number" name="standerd" placeholder="Enter Standerd" required="required"></td></th>
+
+
+    <th>Enter Student Name<td> <input type="text" name="stuname" placeholder="Enter Student Name" required="required"></td></th>
+    <td colspan="2"><input type="submit" name="submit" value="Search"></td>
+</tr>
+</form>
+</table>
+<table align="center" width="80%" border="1"style="margin-top:10px;">
+    <tr style="background-color:#000; color:#fff">
+        <th>No</th>
+        <th>Image</th>
+        <th>Name</th>
+        <th>Rollno</th>
+        <th>Edit</th>
+    </tr>
+    <?php
+    if(isset($_POST['submit']))
+    {
+        include('../dbcon.php');
+        $standerd=$_POST['standerd'];
+        $name=$_POST['stuname'];
+        $sql="SELECT * FROM `student` WHERE `standerd`='$standerd' AND `name` LIKE '%$name%'";
+        $run=mysqli_query($con,$sql);
+        if(mysqli_num_rows($run)<1){
+            echo "<tr><td colspan='5'>No Record Found</td></tr>";
+        }
+        else
+        {
+            $count=0;
+            while($data=mysqli_fetch_assoc($run))
+            {   
+                $count++;
+                ?>
+                 <tr>
+                  <td><?php echo $count;?></td>
+                  <td><img src="../dataimg/<?php echo $data['image']; ?>" style="max-width:100px;"/></td>
+                  <td><?php echo $data['name'];?></td>
+                  <td><?php echo $data['rollno'];?></td>
+                  <td>Edit</td>
+                 </tr>
+
+                <?php
+            }
+        }
+
+
+
+    }
+
+?>
+</table>
